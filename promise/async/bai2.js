e5a869163b761eb0423159ee260729d322401202
namnguyen <namnguyenba6298@gmail.com>
2020-05-31 23:33:53 +0700

    async/bai2

--- promise/async/bai2.js
+++ promise/async/bai2.js
@@ -17,15 +17,35 @@ const promise3 = function (value) {
    return new Promise ((resolve, reject) => {
       if (value > 10) resolve(true)
       reject(false)
-   }).then(data => data)
-     .catch(err => err)
+   })
 }
 async function run() {
-    const num1 = await promise1()
+    const num1 = await promise1();
     const num2 = await promise2(num1)
-    const num3 = await promise3(num2)
-   
-    console.log(num3 + ` - ${Date.now() - time}s`);
-    
+    try {
+        const num3 = await promise3(num2)
+        console.log(num3 + ` - ${Date.now() - time}s`);
+    } catch (error) {
+        throw error;
+    } 
 }
-run();
+run()
+    .then(res => console.log(res))
+    .catch(err => console.log(err))
+
+// const promise3 = function (value) {
+//     return new Promise ((resolve, reject) => {
+//         if (value > 10) resolve(true)
+//         reject(false)
+//     }).then(data => data)
+//       .catch(err => err)
+//     }
+//     async function run() {
+//         const num1 = await promise1()
+//         const num2 = await promise2(num1)
+//         const num3 = await promise3(num2)
+    
+//         console.log(num3 + ` - ${Date.now() - time}s`);
+        
+// }
+// run();
